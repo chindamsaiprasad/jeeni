f2e7241f4771d14e512bb592bb995a53410784ed
chindamsaiprasad <137868328+chindamsaiprasad@users.noreply.github.com>
2024-04-17 17:08:50 +0530

    initial setup

---
+++ android/app/src/main/kotlin/com/example/jeeni/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jeeni
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:jeeni/pages/auth/login_page.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'jeeni',
+      home: LoginPage(),
+    );
+  }
+}
